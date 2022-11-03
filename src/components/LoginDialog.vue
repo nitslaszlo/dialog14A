495364ec4510a3e991b4773f1817cf719b51a0dd
nitslaszlo <nitslaszlo@gmail.com>
2022-11-03 11:36:14 +0100

    óra vége 2022.11.03

---
+++ src/components/LoginDialog.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+  import { reactive } from "vue";
+  interface IProps {
+    email?: string;
+    password?: string;
+  }
+
+  const props = withDefaults(defineProps<IProps>(), {
+    email: "student001@jedlik.eu",
+    password: "student001",
+  });
+
+  interface IReactiveData {
+    email: string;
+    password: string;
+  }
+
+  const r = reactive<IReactiveData>({
+    email: props.email,
+    password: props.password,
+  });
+</script>
+
+<template>
+  <q-dialog>
+    <q-card>
+      <q-input v-model="r.email" />
+    </q-card>
+  </q-dialog>
+</template>
+
+<style lang="scss" scoped></style>
--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -1,69 +1,41 @@
 <script setup lang="ts">
-  import { reactive, computed } from "vue";
+  import { computed, reactive } from "vue";
   import { useUsersStore } from "../store/usersStore";
+  import LoginDialog from "../components/LoginDialog.vue";
 
   const usersStore = useUsersStore();
 
-  const anyLoggedUser = computed(() => (usersStore.getLoggedUser ? true : false));
-
   interface IReactiveData {
-    email: string;
-    password: string;
+    showDialog: boolean;
   }
 
-  const state = reactive<IReactiveData>({
-    email: "student001@jedlik.eu",
-    password: "student001",
+  const r = reactive<IReactiveData>({
+    showDialog: false,
   });
 
-  function Submit() {
-    if (!anyLoggedUser.value) {
-      usersStore.loginUser({
-        email: state.email,
-        password: state.password,
-      });
-    } else {
-      usersStore.logOut();
-    }
-  }
+  const anyLoggedUser = computed(() => (usersStore.getLoggedUser ? true : false));
 </script>
 
 <template>
   <q-page>
-    <div class="row justify-center">
-      <div class="col-12 col-sm-8 col-md-6 col-lg-4 q-gutter-md">
-        <q-form @submit="Submit">
-          <h5 v-if="!anyLoggedUser" class="text-center q-mt-sm q-mb-none">Login</h5>
-          <h5 v-else class="text-center q-mt-sm q-mb-none">Logout</h5>
-          <q-input
-            v-model="state.email"
-            :disable="anyLoggedUser"
-            filled
-            label="e-mail:"
-            :rules="[(v) => (v != null && v != '') || 'Please fill in!']"
-            type="text"
-          />
-          <q-input
-            v-if="!anyLoggedUser"
-            v-model="state.password"
-            filled
-            label="Password:"
-            :rules="[(v) => (v != null && v != '') || 'Please fill in!']"
-            type="text"
-          />
-          <div class="row justify-center">
-            <q-btn
-              class="q-mr-md"
-              color="green"
-              :label="anyLoggedUser ? 'Logout' : 'Login'"
-              no-caps
-              type="submit"
-            />
-          </div>
-        </q-form>
-      </div>
+    <div class="row window-height flex-center justify-evenly">
+      <q-btn
+        class="shadow-10"
+        color="info"
+        :label="anyLoggedUser ? 'Show logout dialog' : 'Show login dialog'"
+        no-caps
+        @click="r.showDialog = true"
+      />
+      <q-btn
+        class="shadow-10"
+        color="blue"
+        :label="anyLoggedUser ? 'Logout from Google' : 'Login/Register with Google'"
+        no-caps
+      />
+      <!-- LoginDialog saját komponens helye -->
+      <LoginDialog email="alma@jedlik.eu" password="körte" />
     </div>
   </q-page>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped></style>
